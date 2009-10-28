738d12f125af3ff46213202f2e29d18cc7d97514
Steve Dekorte <steve@StevesLaptop-7.local>
2009-10-27 22:10:12 -0700

    latest

--- source/VertexServer.c
+++ source/VertexServer.c
@@ -564,7 +564,7 @@ int VertexServer_api_queuePopTo(VertexServer *self)
 			
 			//printf("queueing key %s\n", Datum_data(k));
 			yajl_gen_datum(self->yajl, k);
-			PNode_removeAtCursor(fromNode);
+			PNode_removeAt_(fromNode, k);
 		}
 		else
 		{
