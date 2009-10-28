b23343edf5ba465b01dd4a6e5e93c142ba58a4cd
Steve Dekorte <steve@StevesLaptop-7.local>
2009-10-28 00:55:37 -0700

    added returns to debug output

--- source/VertexServer.c
+++ source/VertexServer.c
@@ -207,7 +207,7 @@ void VertexServer_parseUri_(VertexServer *self, const char *uri)
 	Datum *uriDatum = POOL_ALLOC(self->pool, Datum);
 	Datum_setCString_(uriDatum, uri);
 	
-	if(self->debug) Log_Printf_("request: %s", uri);
+	if(self->debug) Log_Printf_("request: %s\n", uri);
 	
 	CHash_clear(self->query);
 	
