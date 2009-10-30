227447f8fec48e64db5da597839b37b1654554c6
Steve Dekorte <steve@StevesLaptop-7.local>
2009-10-30 03:01:15 -0700

    moved to using a global Pool object, instead of ones in VertexDB and PDB

--- source/pdb/Pool.h
+++ source/pdb/Pool.h
@@ -17,12 +17,17 @@ typedef struct
 typedef void *(PoolNewFunc)(void);
 typedef void (PoolFreeFunc)(void *);
 
+Pool *Pool_globalPool(void);
+void Pool_globalPoolFreeRefs(void);
+void Pool_freeGlobalPool(void);
+
 Pool *Pool_new(void);
 void Pool_free(Pool *self);
 void Pool_freeRefs(Pool *self);
 void *Pool_alllocWithNewAndFree(Pool *self, PoolNewFunc *newFunc, PoolFreeFunc *freeFunc);
 
 #define POOL_ALLOC(self, name) Pool_alllocWithNewAndFree(self, (PoolNewFunc *)name##_new, (PoolFreeFunc *)name##_free)
+#define GLOBAL_POOL_ALLOC(name) POOL_ALLOC(Pool_globalPool(), name);
 
 #ifdef __cplusplus
 }
